9890b296b1c9d7f221ff0507ee8eedff459103f3
Emilio Rodriguez <erdguezh@gmail.com>
2024-12-16 15:16:04 +0000

    modifica a frase para incluir a Emily

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Rocio, si tu me amas, regalame una risita preciosa como tu.</h1>
+    return <h1>Gordita, yo las amo con mi vida y siempre voy a estar aqui, regalame una risita preciosa como tu.</h1>
 }
 
 export default Home;
